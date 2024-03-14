704b8abc7f2d0ccbfdcc1a8ebd39f4d8d87d690a
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2024-03-14 11:12:14 -0600

    :tada: initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+module.exports = config
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
+  mode: 'jit',
+}
+
+export default config
