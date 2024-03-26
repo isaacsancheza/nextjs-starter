951ad6868aa3bccbe79bd9e7161ae147261f5762
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2024-03-26 15:02:32 -0600

    :label: add heroicon type

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,2 @@
 declare module '*.css'
+declare type HeroIcon = React.ForwardRefExoticComponent<React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & { title?: string, titleId?: string } & React.RefAttributes<SVGSVGElement>>
